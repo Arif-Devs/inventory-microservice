431131d410118c626c978f448aad19c9af492311
Arifur Rahman <arifur.sew@gmail.com>
2025-10-29 21:03:51 +0600

    configure route

--- api-gateway/src/utils.ts
+++ api-gateway/src/utils.ts
@@ -1,13 +1,13 @@
 import { Express, Request, Response } from 'express';
 import config from './config.json';
 import axios from 'axios';
+import middlewares from './middlewares';
 
 export const createHandler = (hostname: string, path: string, method: string) => {
   
   return async (req: Request, res: Response) => {
     try {
       let url = `${hostname}${path}`;
-      
       req.params && Object.keys(req.params).forEach((param) => {
           url = url.replace(`:${param}`, req.params[param]);
         });
@@ -18,6 +18,11 @@ export const createHandler = (hostname: string, path: string, method: string) =>
       data: req.body,
       headers: {
         origin: 'http://localhost:8081',
+        	'x-user-id': req.headers['x-user-id'] || '',
+					'x-user-email': req.headers['x-user-email'] || '',
+					'x-user-name': req.headers['x-user-name'] || '',
+					'x-user-role': req.headers['x-user-role'] || '',
+					'user-agent': req.headers['user-agent'],
       },
       });
       res.json(data);
@@ -33,20 +38,26 @@ export const createHandler = (hostname: string, path: string, method: string) =>
     }
   };
 };
+
+export const getMiddlewares = (names: string[]) => {
+	return names.map((name) => middlewares[name]);
+};
+
+
 export const configureRoutes = (app: Express) => {
   Object.entries(config.services).forEach(([_name, service]) => {
     const hostName = service.url;
     service.routes.forEach((route) => {
       route.methods.forEach((method) => {
         //console.log(method, route.path, hostName);
-        
-        const handler = createHandler(hostName, route.path, method);
         const endPoint = `/api${route.path}`;
-        app[method](endPoint, handler);
+        const middleware = getMiddlewares(route.middlewares)
+        const handler = createHandler(hostName, route.path, method);
+        app[method](endPoint, middleware, handler);
       });
     });
   });
 };
-function next(error: unknown) {
-  throw new Error('Function not implemented.');
-}
+// function next(error: unknown) {
+//   throw new Error('Function not implemented.');
+// }
